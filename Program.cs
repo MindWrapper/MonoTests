cb96c6bed5c8eefaac076c60b3ccd114e90ddad7
Yan Drugalya <ydrugalya@gmail.com>
2017-01-05 16:04:57 +0100

    initial project structure

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ProcessExitCode
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var process = Process.Start("bash");
+			Thread.Sleep(500);
+			process.Kill();
+			process.WaitForExit();
+			Console.WriteLine("Exitcode are: " + process.ExitCode);
+		}
+	}
+}
