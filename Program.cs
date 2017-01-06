66dcc86963c4d6031b1b320a0ae317b997caaae1
Yan Drugalya <ydrugalya@gmail.com>
2017-01-06 10:24:07 +0100

    added code to test GDI plus libs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
 using System.Threading;
 
 namespace ProcessExitCode
@@ -8,11 +11,29 @@ namespace ProcessExitCode
 	{
 		public static void Main(string[] args)
 		{
+			if (args.Contains("Process"))
+			{
+				ProccessTest();
+			}
+
+			if (args.Contains("gdi"))
+			{
+				GDIPlusTest();
+			}
+		}
+
+		private static void ProccessTest()
+		{
 			var process = Process.Start("bash");
 			Thread.Sleep(500);
 			process.Kill();
 			process.WaitForExit();
 			Console.WriteLine("Exitcode are: " + process.ExitCode);
+		}
+
+		private static void GDIPlusTest()
+		{
+			var image = Bitmap.FromFile ("test.bmp");
 		}
 	}
 }
